d5fd3ea8e9cb0e0624d1b2b84761d75e24165d2f
obozdag <obozdag@gmail.com>
2020-05-06 14:31:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,53 @@
-var cacheName = 'dua-v1.1';
+var cacheName = 'EasyDua v1.2';
 var staticContentToCache = [
 	'index.html',
 	'favicon.ico',
-	'css/dua.css',
+	'css/easy_dua.css',
 	'css/fonts/hamdullah.ttf',
 	'css/fonts/lateef.ttf',
 	'css/fonts/rb_icons.ttf',
-	'css/icons/dua_32x32.png',
-	'css/icons/dua_48x48.png',
-	'css/icons/dua_64x64.png',
-	'css/icons/dua_72x72.png',
-	'css/icons/dua_96x96.png',
-	'css/icons/dua_128x128.png',
-	'css/icons/dua_144x144.png',
-	'css/icons/dua_152x152.png',
-	'css/icons/dua_192x192.png',
-	'css/icons/dua_384x384.png',
-	'css/icons/dua_512x512.png',
+	'css/icons/easy_dua_32x32.png',
+	'css/icons/easy_dua_48x48.png',
+	'css/icons/easy_dua_64x64.png',
+	'css/icons/easy_dua_72x72.png',
+	'css/icons/easy_dua_96x96.png',
+	'css/icons/easy_dua_128x128.png',
+	'css/icons/easy_dua_144x144.png',
+	'css/icons/easy_dua_152x152.png',
+	'css/icons/easy_dua_192x192.png',
+	'css/icons/easy_dua_384x384.png',
+	'css/icons/easy_dua_512x512.png',
 	'css/icons/loading.gif',
 	'app.js',
 	'js/swipe.js',
 	'js/lang.js',
 	'js/settings.js',
-	'js/dua.js',
+	'js/easy_dua.js',
 	'languages/en/program_info.html',
 	'languages/tr/program_info.html',
 ];
 
 // Installing Service Worker
 self.addEventListener('install', evt => {
-	console.log('Service worker installed.');
 	evt.waitUntil(
 		caches.open(cacheName).then(cache => {
-			staticContentToCache.forEach(function(file){
-				cache.add(file).catch((err)=>{
-					console.error(err)
-				})
-			});
+			return staticContentToCache.forEach(function(file){
+				cache.add(file).catch(err => console.log(err+file))
+			})
 		})
-	);
-});
+		.then(function(){return self.skipWaiting()})
+	)})
+
+// Activating Service Worker
+self.addEventListener('activate', evt => {
+	evt.waitUntil(
+		caches.keys().then(keys => {
+			return Promise.all(keys
+				.filter(key => key !== cacheName)
+				.map(key => caches.delete(key))
+			)
+	}))})
+
 
 // Fetching content using Service Worker
 self.addEventListener('fetch', evt => {
@@ -47,5 +55,5 @@ self.addEventListener('fetch', evt => {
 		caches.match(evt.request).then(
 			cacheResponse => {
 				return cacheResponse || fetch(evt.request);
-		}));
-});
+		}))
+	})
