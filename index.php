8e54061bc65d780ecdc0a8328ea1faf97a3a757b
obozdag <obozdag@gmail.com>
2024-05-01 15:38:59 +0200

    Delete source/cevsen.txt
    To keep only one source of cevsen, cevsen.txt is deleted

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version     = 'v1.31';
+	$version     = 'v1.3.21';
 	$color       = 'steelblue';
 	$pdo         = new PDO('sqlite:db/cevsen.db');
 	$rows_cevsen = $pdo->query('SELECT * FROM fkl_cevsen');
